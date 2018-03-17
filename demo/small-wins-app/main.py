93b383d52bdc0ba31b1f7c4f0ae4b66422b58ddb
Aaron <aaron@webappio.com>
2018-03-17 22:34:43 +0000

    Adds templates to make use of the app.

--- demo/small-wins-app/main.py
+++ demo/small-wins-app/main.py
@@ -11,36 +11,54 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import os
 import webapp2
+import logging
+from google.appengine.ext.webapp import template
+from google.appengine.ext import ndb
+from google.appengine.api import users
 
 from twilio.rest import Client
 
-from google.appengine.ext import ndb
 from env_variables import ACCOUNT_SID, AUTH_TOKEN, SERVICE_NUMBER
 
-class RegisteredUser(ndb.Model):
+class RegisteredUsers(ndb.Model):
     name = ndb.StringProperty(default="")
     number = ndb.StringProperty(default="")
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
 
+def renderTemplate(template_name, values={}):
+    path = os.path.join(os.path.dirname(__file__), 'templates/', template_name)
+    template_values = {}
+    response = template.render(path, values)
+    return response
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Welcome to Small Wins!')
-
+        template = renderTemplate(template_name="index.html")
+        return self.response.out.write(template)
 
 class RegisterNumberHandler(webapp2.RequestHandler):
     def post(self):
-        return self.abort(501)
+        new_user = RegisteredUsers()
+        new_user.number = str(self.request.get("inbound"))
+        new_user.name = str(self.request.get("body")).strip().split(" ")[0]
+        new_user.put()
+        return self.response.write('Ok!')
 
 
 class BroadcastMessageHandler(webapp2.RequestHandler):
     def get(self):
+        users = RegisteredUsers.query()
+        template = renderTemplate(template_name="broadcast.html", values=users)
+        return self.response.out.write(template)
+
+
+    def post(self):
         client = Client(ACCOUNT_SID, AUTH_TOKEN)
         message = self.request.get("message")
-        numbers = RegisteredUser.query()
+        numbers = RegisteredUsers.query()
         for user in numbers:
             to_number = user.number
             message = client.messages.create(
@@ -55,6 +73,6 @@ class BroadcastMessageHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/register' + '/?(.*)?', RegisterNumberHandler),
-    ('/broadcast' + '/?(.*)?', BroadcastMessageHandler),
+    ('/register', RegisterNumberHandler),
+    ('/broadcast', BroadcastMessageHandler),
 ], debug=True)
